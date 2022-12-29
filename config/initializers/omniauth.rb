e205f75667d7e92e7a1ef003a227137be0d6346a
tamashiro-syuta <syuta5825@gmail.com>
2022-12-29 17:31:29 +0900

    🛂 oauth用のミドルウェアを作成

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,11 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  # 学習用のアプリなので、環境変数を変えずに同じものを使用
+  if Rails.env.development? || Rails.env.test?
+    provider :github,
+    Rails.application.credentials.github[:client_id],
+    Rails.application.credentials.github[:client_secret]
+  else
+    provider :github,
+    Rails.application.credentials.github[:client_id],
+    Rails.application.credentials.github[:client_secret]
+end
