d5ca9407308f9c793e43961ba86ac2b89779bc5a
tamashiro-syuta <syuta5825@gmail.com>
2022-12-30 11:10:02 +0900

    ✨ ログイン機能の完成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  def self.find_or_create_from_auth_hash!(auth_hash)
+    provider = auth_hash[:provider]
+    uid = auth_hash[:uid]
+    nickname = auth_hash[:info][:nickname]
+    image_url = auth_hash[:info][:image]
+
+    # find_or_create_by : 引数に設定した値(ここではproviderとuid)に一致するレコードがない場合は、作成
+    User.find_or_create_by!(provider: provider, uid: uid) do |user|
+      user.name = nickname
+      user.image_url = image_url
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # ログイン後のcallback先とアクションを紐付ける
+  get "/auth/:provider/callback" => "sessions#create"
 end
