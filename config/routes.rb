a0774c6cb40a403b62af095fc3508b29f062de82
tamashiro-syuta <syuta5825@gmail.com>
2022-12-31 13:13:45 +0900

    ✨ rails g resource event

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-
   # ログイン後のcallback先とアクションを紐付ける
   get "/auth/:provider/callback" => "sessions#create"
   delete "/logout" => "sessions#destroy"
+
+  resources :events
 end
