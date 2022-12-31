deb20b20318d6001c5c1d3b49bcb833727dd153d
tamashiro-syuta <syuta5825@gmail.com>
2022-12-31 13:14:11 +0900

    🦺 eventsのバリデーションを追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,18 @@
 class Event < ApplicationRecord
+  validates :name, length: { maximum: 50 }, presence: true
+  validates :place, length: { maximum: 100 }, presence: true
+  validates :content, length: { maximum: 2000 }, presence: true
+  varidates :start_at, presence: true
+  varidates :end_at, presence: true
+  validates :start_at_should_be_before_end_at
+
+  private
+
+  # start_atはend_atよりも前の時間
+  def start_at_should_be_before_end_at
+    return unless start_at && end_at
+    if start_at >= end_at
+      errors.add(:start_at, "は終了時間よりも前に設定してください")
+    end
+  end
 end
