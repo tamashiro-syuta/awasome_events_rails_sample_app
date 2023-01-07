2389e7194088143df2d7ad2dc9756066ba1e5dda
tamashiro-syuta <syuta5825@gmail.com>
2023-01-07 13:58:30 +0900

    💄 イベント一覧を作成

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
   # authenticateをスキップ(ログイン状態でなくても、このcontrollerの処理は行える)
   skip_before_action :authenticate
+
   def index
+    @events = Event.where("start_at > ?", Time.zone.now).order(:start_at)
   end
 end
