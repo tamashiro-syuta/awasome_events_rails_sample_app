ba1771ff306885f0e13572d346d3810ae99ed850
tamashiro-syuta <syuta5825@gmail.com>
2023-01-07 15:38:03 +0900

    ✨ 参加機能の作成

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,20 @@
 class TicketsController < ApplicationController
+  def new
+    # ユーザーが直接URLを入力した時にルーティングエラーにする
+    # ※未ログイン時は、beforeのauthenticateメソッドで先にログインさせる挙動なので本来はこのメソッドが呼ばれることはない
+    raise ActionController::RoutingError, "ログイン状態でTicketController#newにアクセス"
+  end
+
+  def create
+    puts "params"
+    puts params
+    event = Event.find(params[:event_id])
+    @ticket = current_user.tickets.build do |t|
+      t.event = event
+      t.comment = params[:ticket][:comment]
+    end
+    if @ticket.save
+      redirect_to event, notice: "このイベントに参加表明しました"
+    end
+  end
 end
