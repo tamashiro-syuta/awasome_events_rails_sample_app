c204e5fdaab94e01ded5f33f064f123dda745ebb
tamashiro-syuta <syuta5825@gmail.com>
2023-01-07 17:00:15 +0900

    ✨ 退会処理の追加

--- app/controllers/retirements_controller.rb
+++ app/controllers/retirements_controller.rb
@@ -1,2 +1,12 @@
 class RetirementsController < ApplicationController
+  def new
+  end
+
+  def create
+    if current_user.destroy
+      # セッションも削除し、ログアウト後と同じ状態にする
+      reset_session
+      redirect_to root_path, notice: "退会完了しました"
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ApplicationRecord
+  # 削除前にバリデーションを走らせる
+  before_destroy :check_all_events_finished
+
   # ユーザーが作成したイベントだと理解しやすいように、created_eventsという名前で定義(通常なら、モデル名であるeventにする)
   # モデル名と異なる関連付けを追加したので、オプションのclass_nameで、どのクラスと紐つけるのか指定
   # 外部キーも、デフォルトのuser_idでないカラムと紐つけるので、オプションのforeign_keyで、紐付けるカラムを指定
-  has_many :created_events, class_name: "Event", foreign_key: "owner_id"
-  has_many :tickets
+  # dependent: :nullify は、削除時に関連するレコードの外部キーをnullにする
+  has_many :created_events, class_name: "Event", foreign_key: "owner_id", dependent: :nullify
+  has_many :tickets, dependent: :nullify
+  # check_all_events_finishedメソッドの中で、参加しているイベントを調べるために、participating_eventsを定義
+  has_many :participating_events, through: :tickets, source: :event
 
   def self.find_or_create_from_auth_hash!(auth_hash)
     provider = auth_hash[:provider]
@@ -16,5 +22,20 @@ class User < ApplicationRecord
       user.name = nickname
       user.image_url = image_url
     end
+  end
+
+  private
+
+  def check_all_events_finished
+    now = Time.zone.now
+    if created_events.where(":now < end_at", now: now).exists?
+      errors[:base] << "公開中の未終了イベントが存在します。"
+    end
+
+    if participating_events.where(":now < end_at", now: now).exists?
+      errors[:base] << "未終了の参加イベントが存在します。"
+    end
+
+    throw(:abort) unless errors.empty?
   end
 end
