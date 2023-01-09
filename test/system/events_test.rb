cbe4442fff3ddbd49c5b8cc9d1b49700004f622c
tamashiro-syuta <syuta5825@gmail.com>
2023-01-09 23:21:55 +0900

    ✅ イベント用ページのテストを追加

---
+++ test/system/events_test.rb
@@ -0,0 +1,18 @@
+require "application_system_test_case"
+
+class EventsTest < ApplicationSystemTestCase
+  test "/events/:id ページを表示" do
+    event = FactoryBot.create(:event)
+    visit event_url(event)
+
+    assert_selector "h1", text: event.name
+  end
+
+  test "/events/new ページを表示" do
+    sign_in_as(FactoryBot.create(:user))
+    event = FactoryBot.create(:event)
+    visit new_event_url
+
+    assert_selector "h1", text: "イベント作成"
+  end
+end
