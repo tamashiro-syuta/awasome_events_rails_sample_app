ad1c2ccf7c104826eebf4125baf10ff517bf42d6
tamashiro-syuta <syuta5825@gmail.com>
2023-01-10 09:45:55 +0900

    ✅ 他の人が作ったイベントは削除できないことのテスト

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -9,4 +9,18 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
       delete event_url(event)
     end
   end
+
+  test "他の人が作ったイベントは削除できない" do
+    event_owner = FactoryBot.create(:user)
+    event = FactoryBot.create(:event, owner: event_owner)
+    sign_in_user = FactoryBot.create(:user)
+    sign_in_as sign_in_user
+    # DELETEのリクエストを送っても、イベントの数は変わらない(= 削除できない)
+    assert_difference("Event.count", 0) do
+      # ブロックで指定した動作(DELETEのリクエストを送る)を行うと、引数に指定した例外をキャッチする
+      assert_raises(ActiveRecord::RecordNotFound)do
+        delete event_url(event)
+      end
+    end
+  end
 end
