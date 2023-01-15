da5fc26e4e96118ccfcafd968fb06c1f501764df
tamashiro-syuta <syuta5825@gmail.com>
2023-01-15 12:18:17 +0900

    ✅ モックを使ったテストを追加

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  test "#created_by? owner_idと引数の#idが同じ時" do
+  test "#created_by? owner_idと引数の#idが同じ時(スタブver)" do
     event = FactoryBot.create(:event)
     user = User.new
     # Eventモデルのテストなので、Userモデルのオブジェクトを使ってテストをすると、Userモデルの実装に依存したテストになるので、スタブ化したオブジェクトを使用し、それぞれの結合を疎にする
@@ -11,6 +11,17 @@ class EventTest < ActiveSupport::TestCase
       # スタブ化されたuserオブジェクトでテスト
       assert_equal(true, event.created_by?(user))
     end
+  end
+
+  test "#created_by? owner_idと引数の#idが同じ時(モックver)" do
+    event = FactoryBot.create(:event)
+    # モックは、戻り値の変更に加えて、そのメソッド(下ではidメソッドが呼ばれる)が呼び出されたかを確認できる(user.verify)
+
+    # idにevent.owner_idを返すモックを生成し、userに格納(user.id = 1 になる)
+    user = MiniTest::Mock.new.expect(:id, event.owner_id)
+    assert_equal(true, event.created_by?(user))
+    # モックオブジェクトのidメソッドが呼び出されたかを確認(これがないとエラーになる)
+    user.verify
   end
 
   test "#created_by? owner_idと引数の#idが異なる時" do
